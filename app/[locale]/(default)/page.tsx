c9e651e541a4841fa9253822a17673b76774ded2
Archit18th <archit@18thdigitech.net>
2024-06-10 09:36:18 +0530

    carousel

--- app/[locale]/(default)/page.tsx
+++ app/[locale]/(default)/page.tsx
@@ -63,7 +63,7 @@ export default async function Home({ params: { locale } }: Props) {
 
   return (
     <>
-      {/* <Hero /> */}
+      <Hero />
       <CustomCarousel />
       <div className="my-10">
         <NextIntlClientProvider locale={locale} messages={{ Product: messages.Product ?? {} }}>
